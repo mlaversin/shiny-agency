f28324e437669cb24dd95f4feefe38f856691dff
Morgan Laversin <morganlaversin@gmail.com>
2022-09-26 21:27:15 +0200

    add a dark mode (context)

--- src/index.js
+++ src/index.js
@@ -6,41 +6,36 @@ import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Error from './components/Error'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-
-    body {
-      margin: 0;
-    }
-`
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider } from './utils/context'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/survey/:questionNumber">
-          <Survey />
-        </Route>
-        <Route path="/results">
-          <Results />
-        </Route>
-        <Route path="/freelances">
-          <Freelances />
-        </Route>
-        <Route>
-          <Error />
-        </Route>
-      </Switch>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/survey/:questionNumber">
+            <Survey />
+          </Route>
+          <Route path="/results">
+            <Results />
+          </Route>
+          <Route path="/freelances">
+            <Freelances />
+          </Route>
+          <Route>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
